3c650ed0c0956e1f9fa3a5e438f689537eef84c1
lijianliang1997 <940104151@qq.com>
2018-06-14 21:11:18 +0800

    Initial commit

---
+++ src/yxxy/c_023/Singleton.java
@@ -0,0 +1,31 @@
+
+package yxxy.c_023;
+
+import java.util.Arrays;
+
+public class Singleton {
+	
+	private Singleton() {
+		System.out.println("single");
+	}
+	
+	private static class Inner {
+		private static Singleton s = new Singleton();
+	}
+	
+	public static Singleton getSingle() {
+		return Inner.s;
+	}
+	
+	public static void main(String[] args) {
+		Thread[] ths = new Thread[200];
+		for(int i=0; i<ths.length; i++) {
+			ths[i] = new Thread(()->{
+				Singleton.getSingle();
+			});
+		}
+		
+		Arrays.asList(ths).forEach(o->o.start());
+	}
+	
+}
---
+++ src/yxxy/c_026/T13_ThreadPoolExecutor.java
@@ -0,0 +1,9 @@
+
+package yxxy.c_026;
+
+
+public class T13_ThreadPoolExecutor {
+	public static void main(String[] args) {
+		
+	}
+}
